97cdcd24ca39880506992a9d94d9dcb4fb931168
lokesh <lokesh.kushwah@gmail.com>
2019-10-16 15:54:33 +0530

    create separate folder for header, footer,navigation and show in index1.php, and set form joint-development.php and channel-partner.php page

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php
+$title = 'About';
+$page = "about-us";
+$meta_keywords = "keyword";
+$meta_description = "description";
+include 'includes/header.php';
+?>
+<div role="main" class="main">
+  <section class="page-header page-header-modern page-header-background page-header-background-sm overlay overlay-color-primary overlay-show overlay-op-8 mb-5" style="background-image: url(img/page-header/page-header-elements.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 align-self-center p-static order-2 text-center">
+                    <h1>About</h1>
+
+                </div>
+                <div class="col-md-12 align-self-center order-1">
+                    <ul class="breadcrumb breadcrumb-light d-block text-center">
+                        <li><a href="#">Home</a></li>
+                        <li class="active">About</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php //content show here ?>
+
+</div>
+
+<?php
+include 'includes/footer.php';
---
+++ index1.php
@@ -0,0 +1,335 @@
+<?php
+$title='home page';
+$page="home";
+$meta_keywords="keyword";
+$meta_description="description";
+include 'includes/header.php';
+?>
+<div role="main" class="main">
+				<div class="slider-container light rev_slider_wrapper" style="height: 700px;">
+					<div id="revolutionSlider" class="slider rev_slider" data-version="5.4.8" data-plugin-revolution-slider data-plugin-options="{'delay': 9000, 'gridwidth': 1170, 'gridheight': 700, 'disableProgressBar': 'on'}">
+						<ul>
+							<li data-transition="fade">
+								<img src="img/demos/construction/slides/slide-construction-1.jpg"  
+									alt=""
+									data-bgposition="center 100%" 
+									data-bgfit="cover" 
+									data-bgrepeat="no-repeat" 
+									class="rev-slidebg">
+								
+								<div class="tp-caption top-label tp-caption-custom-stripe"
+									data-x="right" data-hoffset="100"
+									data-y="bottom" data-voffset="106"
+									data-start="1000"
+									data-transform_in="x:[100%];opacity:0;s:1000;">#1 FASTEST-GROWING COMPANY</div>
+							</li>
+							<li data-transition="fade">
+								<img src="img/demos/construction/slides/slide-construction-2.jpg"  
+									alt=""
+									data-bgposition="center 100%" 
+									data-bgfit="cover" 
+									data-bgrepeat="no-repeat" 
+									class="rev-slidebg">
+								
+								<div class="tp-caption top-label tp-caption-custom-stripe"
+									data-x="right" data-hoffset="180"
+									data-y="bottom" data-voffset="106"
+									data-start="1000"
+									data-transform_in="x:[100%];opacity:0;s:1000;">MODERN OFFICES DESIGN</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="container">
+					<div class="row mt-4 mt-lg-5 mb-4 py-4">
+						<div class="col-lg-4">
+							<h2 class="mb-0 text-color-dark">Who We Are</h2>
+							<p class="lead">Lorem ipsum dolor sit amet.</p>
+						</div>
+						<div class="col-lg-2 text-center d-none d-lg-block">
+							<img src="img/dotted-line-angle.png" class="img-fluid" alt="" />
+						</div>
+						<div class="col-lg-6">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
+							<a class="mt-3" href="demo-construction-company.html">Learn More <i class="fas fa-long-arrow-alt-right"></i></a>
+						</div>
+					</div>
+				</div>
+
+				<section class="section section-tertiary section-no-border section-custom-construction">
+					<div class="container">
+						<div class="row pt-4">
+							<div class="col">
+								<h2 class="mb-0 text-color-dark">Services</h2>
+								<p class="lead">Lorem ipsum dolor sit amet.</p>
+							</div>
+						</div>
+
+						<div class="row mt-4">
+							<div class="col-lg-6">
+								<div class="feature-box feature-box-style-2 custom-feature-box mb-4 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="300">
+									<div class="feature-box-icon w-auto">
+										<img src="img/demos/construction/icons/ground-construction.png" alt="" class="img-fluid" width="55" />
+									</div>
+									<div class="feature-box-info ml-3">
+										<h4 class="mb-2">Pre-Construction</h4>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
+										<a class="mt-3" href="demo-construction-services-detail.html">Learn More <i class="fas fa-long-arrow-alt-right"></i></a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="feature-box feature-box-style-2 custom-feature-box mb-4 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="0">
+									<div class="feature-box-icon w-auto">
+										<img src="img/demos/construction/icons/construction.png" alt="" class="img-fluid" width="55" />
+									</div>
+									<div class="feature-box-info ml-3">
+										<h4 class="mb-2">General Construction</h4>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum pellentesque imperdiet. Nulla lacinia iaculis nulla.</p>
+										<a class="mt-3" href="demo-construction-services-detail.html">Learn More <i class="fas fa-long-arrow-alt-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="row mt-3 mb-4">
+							<div class="col-lg-6">
+								<div class="feature-box feature-box-style-2 custom-feature-box mb-4 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="300">
+									<div class="feature-box-icon w-auto">
+										<img src="img/demos/construction/icons/painting.png" alt="" class="img-fluid" width="55" />
+									</div>
+									<div class="feature-box-info ml-3">
+										<h4 class="mb-2">Painting</h4>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+										<a class="mt-3" href="demo-construction-services-detail.html">Learn More <i class="fas fa-long-arrow-alt-right"></i></a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="feature-box feature-box-style-2 custom-feature-box mb-4 appear-animation" data-appear-animation="fadeInUp" data-appear-animation-delay="0">
+									<div class="feature-box-icon w-auto">
+										<img src="img/demos/construction/icons/plumbing.png" alt="" class="img-fluid" width="55" />
+									</div>
+									<div class="feature-box-info ml-3">
+										<h4 class="mb-2">Plumbing</h4>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+										<a class="mt-3" href="demo-construction-services-detail.html">Learn More <i class="fas fa-long-arrow-alt-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="pt-3 pb-4 home-concept-construction">
+					<div class="container">
+						<div class="row pt-4">
+							<div class="col">
+								<h2 class="mb-0 text-color-dark">Projects</h2>
+								<p class="lead">Lorem ipsum dolor sit amet.</p>
+
+								<div class="diamonds-wrapper lightbox" data-plugin-options="{'delegate': '.diamond', 'type': 'image', 'gallery': {'enabled': true}}">
+									<ul class="diamonds">
+										<li>
+											<a href="img/demos/construction/gallery/construction-gallery-1.jpg" class="diamond">
+												<div class="content">
+													<img src="img/demos/construction/gallery/construction-gallery-1.jpg" class="img-fluid" alt="" />
+												</div>
+											</a>
+										</li>
+										<li>
+											<a href="img/demos/construction/gallery/construction-gallery-2.jpg" class="diamond">
+												<div class="content">
+													<img src="img/demos/construction/gallery/construction-gallery-2.jpg" class="img-fluid" alt="" />
+												</div>
+											</a>
+										</li>
+										<li>
+											<a href="img/demos/construction/gallery/construction-gallery-3.jpg" class="diamond">
+												<div class="content">
+													<img src="img/demos/construction/gallery/construction-gallery-3.jpg" class="img-fluid" alt="" />
+												</div>
+											</a>
+										</li>
+										<li>
+											<a href="img/demos/construction/gallery/construction-gallery-4.jpg" class="diamond diamond-sm">
+												<div class="content">
+													<img src="img/demos/construction/gallery/construction-gallery-4.jpg" class="img-fluid" alt="" />
+												</div>
+											</a>
+										</li>
+										<li>
+											<a href="img/demos/construction/gallery/construction-gallery-5.jpg" class="diamond">
+												<div class="content">
+													<img src="img/demos/construction/gallery/construction-gallery-5.jpg" class="img-fluid" alt="" />
+												</div>
+											</a>
+										</li>
+										<li>
+											<a href="img/demos/construction/gallery/construction-gallery-6.jpg" class="diamond diamond-sm">
+												<div class="content">
+													<img src="img/demos/construction/gallery/construction-gallery-6.jpg" class="img-fluid" alt="" />
+												</div>
+											</a>
+										</li>
+										<li>
+											<a href="img/demos/construction/gallery/construction-gallery-7.jpg" class="diamond diamond-sm">
+												<div class="content">
+													<img src="img/demos/construction/gallery/construction-gallery-7.jpg" class="img-fluid" alt="" />
+												</div>
+											</a>
+										</li>
+									</ul>
+								</div>
+
+							</div>
+						</div>
+						<div class="row row-diamonds-description justify-content-center justify-content-xl-start text-center text-xl-left">
+							<div class="col-md-8 col-lg-6">
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing metus elit. Quisque rutrum pellentesque imperdiet.</p>
+								<a class="btn btn-outline btn-primary" href="demo-construction-projects.html">View All Projects</a>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="section section-background mb-4" style="background-image: url(img/demos/construction/testimonials/testimonials-bg.jpg); background-position: 50% 100%; min-height: 540px; background-size: cover;">
+					<div class="container">
+						<div class="row justify-content-end">
+							<div class="col-lg-6">
+								
+								<div class="owl-carousel owl-theme nav-bottom rounded-nav mt-4 pt-4 mb-4 pb-4" data-plugin-options="{'items': 1, 'loop': false}">
+									<div>
+										<div class="col">
+											<div class="testimonial testimonial-style-2 testimonial-with-quotes mb-0">
+												<blockquote>
+													<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget risus porta, tincidunt turpis at, interdum tortor. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce ante tellus, convallis non consectetur sed, pharetra nec ex.</p>
+												</blockquote>
+												<div class="testimonial-author">
+													<p><strong>Ana Smith</strong><span>Porto Magazine</span></p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div>
+										<div class="col">
+											<div class="testimonial testimonial-style-2 testimonial-with-quotes mb-0">
+												<blockquote>
+													<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget risus porta, tincidunt turpis at, interdum tortor. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+												</blockquote>
+												<div class="testimonial-author">
+													<p><strong>Jessica Smith</strong><span>Porto Magazine</span></p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<section class="pt-3 section-custom-construction-2">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-12 text-center">
+								<div class="owl-carousel owl-theme stage-margin rounded-nav" data-plugin-options="{'margin': 10, 'loop': true, 'nav': true, 'dots': false, 'stagePadding': 40, 'items': 6, 'autoplay': true, 'autoplayTimeout': 3000}">
+									<div>
+										<img class="img-fluid" src="img/logos/logo-1.png" alt="">
+									</div>
+									<div>
+										<img class="img-fluid" src="img/logos/logo-2.png" alt="">
+									</div>
+									<div>
+										<img class="img-fluid" src="img/logos/logo-3.png" alt="">
+									</div>
+									<div>
+										<img class="img-fluid" src="img/logos/logo-4.png" alt="">
+									</div>
+									<div>
+										<img class="img-fluid" src="img/logos/logo-5.png" alt="">
+									</div>
+									<div>
+										<img class="img-fluid" src="img/logos/logo-6.png" alt="">
+									</div>
+									<div>
+										<img class="img-fluid" src="img/logos/logo-4.png" alt="">
+									</div>
+									<div>
+										<img class="img-fluid" src="img/logos/logo-2.png" alt="">
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-12 text-center mt-4">
+								<hr class="solid mt-0 mb-4">
+							</div>
+						</div>
+						<div class="row pt-4">
+							<div class="col">
+								<h2 class="mb-0 text-color-dark">Blog</h2>
+								<p class="lead mb-2">Lorem ipsum dolor sit amet.</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-4">
+								<div class="recent-posts mt-4">
+									<a href="demo-construction-blog-detail.html">
+										<img class="img-fluid pb-3" src="img/demos/construction/blog/blog-construction-1.jpg" alt="Blog">
+									</a>
+									<article class="post">
+										<div class="post-date">
+											<span class="day">15</span>
+											<span class="month">Jan</span>
+										</div>
+										<h4 class="pt-2 pb-0 mb-0"><a class="text-color-dark" href="demo-construction-blog-detail.html">Lorem ipsum dolor sit amet</a></h4>
+										<p>By admin</p>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit vehicula est, in consequat libero.</p>
+										<a class="mt-3" href="demo-construction-blog-detail.html">Read More <i class="fas fa-long-arrow-alt-right position-relative top-1"></i></a>
+									</article>
+								</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="recent-posts mt-4">
+									<a href="demo-construction-blog-detail.html">
+										<img class="img-fluid pb-3" src="img/demos/construction/blog/blog-construction-2.jpg" alt="Blog">
+									</a>
+									<article class="post">
+										<div class="post-date">
+											<span class="day">15</span>
+											<span class="month">Jan</span>
+										</div>
+										<h4 class="pt-2 pb-0 mb-0"><a class="text-color-dark" href="demo-construction-blog-detail.html">Lorem ipsum dolor sit amet</a></h4>
+										<p>By admin</p>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit vehicula est, in consequat libero.</p>
+										<a class="mt-3" href="demo-construction-blog-detail.html">Read More <i class="fas fa-long-arrow-alt-right position-relative top-1"></i></a>
+									</article>
+								</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="recent-posts mt-4">
+									<a href="demo-construction-blog-detail.html">
+										<img class="img-fluid pb-3" src="img/demos/construction/blog/blog-construction-3.jpg" alt="Blog">
+									</a>
+									<article class="post">
+										<div class="post-date">
+											<span class="day">15</span>
+											<span class="month">Jan</span>
+										</div>
+										<h4 class="pt-2 pb-0 mb-0"><a class="text-color-dark" href="demo-construction-blog-detail.html">Lorem ipsum dolor sit amet</a></h4>
+										<p>By admin</p>
+										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec hendrerit vehicula est, in consequat libero.</p>
+										<a class="mt-3" href="demo-construction-blog-detail.html">Read More <i class="fas fa-long-arrow-alt-right position-relative top-1"></i></a>
+									</article>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+
+<?php
+include 'includes/footer.php';
