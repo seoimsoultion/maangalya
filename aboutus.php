f36dfc95168912fc2eaf2c063e53838f0bddc8f1
Hamalton <hamalton.xavier@gmail.com>
2019-10-17 11:51:33 +0530

    changes

--- aboutus.php
+++ aboutus.php
@@ -12,9 +12,7 @@
 		<meta name="description" content="Porto - Responsive HTML5 Template">
 		<meta name="author" content="okler.net">
 
-		<!-- Favicon -->
-		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-		<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
 
 		<!-- Mobile Metas -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
