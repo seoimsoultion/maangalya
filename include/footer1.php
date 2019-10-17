11e79feb2bd3af1393288305dc53e70724b113a3
maneeshjohn123 <maneesh@imsolutions.mobi>
2019-10-17 17:37:44 +0530

    maneesh

---
+++ include/footer1.php
@@ -0,0 +1,145 @@
+
+			<footer id="footer">
+				<div class="container">
+					<div class="row pt-4 pb-5 text-center text-md-left">
+						<div class="col-md-3">
+							<a href="demo-construction.html">
+								<img alt="Porto" class="img-fluid logo" width="200" src="img/new_images/signature-logo.png">
+							</a>
+							<p>MAANGALYA PROSPER - SIGNATURE BLOCK <br>
+								Sy. No. 57/4, Anjanapura 3rd ‘A’ Block, <br>Uttarahalli Hobli, JP Nagar 9th Phase, <br>Bangalore 560 108
+</p>
+						</div>
+						<div class="col-md-4">
+							<div class="row">
+								<div class="col-lg-6 mb-2">
+									<h4>Navigation</h4>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-6 mb-0">
+									<ul class="list list-footer-nav">
+										<li>
+											<a class="nav-link" href="demo-construction.html">
+												Home
+											</a>
+										</li>
+										<li>
+											<a class="nav-link" href="aboutus.php">
+															About Us
+														</a>
+										</li>
+										<li>
+											<a class="nav-link" href="#">
+															Why Maangalya
+														</a>
+										</li>
+									</ul>
+								</div>
+								<div class="col-6">
+									<ul class="list list-footer-nav">
+										<li>
+											<a class="nav-link" href="#">
+															Joint Development
+														</a>
+										</li>
+										<li>
+											<a class="nav-link" href="#">
+															Channel Partners
+														</a>
+										</li>
+										<li>
+											<a class="nav-link" href="contact.php">
+															Contact
+														</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-5">
+							<h4>Newsletter</h4>
+
+							<div class="newsletter">
+
+								<div class="alert alert-success d-none" id="newsletterSuccess">
+									<strong>Success!</strong> You've been added to our email list.
+								</div>
+
+								<div class="alert alert-danger d-none" id="newsletterError"></div>
+
+								<form id="newsletterForm" action="php/newsletter-subscribe.php" method="POST">
+									<div class="input-group">
+										<input class="form-control form-control-sm" placeholder="Email Address" name="newsletterEmail" id="newsletterEmail" type="text">
+										<span class="input-group-append">
+											<button class="btn btn-light" type="submit"><i class="icon-paper-plane icons"></i></button>
+										</span>
+									</div>
+								</form>
+
+							</div>
+
+							<p><i class="fas fa-phone"></i> (123) 456-789 <i class="far fa-envelope ml-2"></i> <a href="mailto:mail@example.com">mail@example.com</a></p>
+
+						</div>
+					</div>
+
+					<div class="footer-copyright">
+						<div class="row">
+							<div class="col-lg-12 text-center">
+								<p>© Copyright 2019. All Rights Reserved.</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</footer>
+
+		</div>
+
+		<!-- Vendor -->
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/jquery.appear/jquery.appear.min.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.min.js"></script>
+		<script src="vendor/jquery.cookie/jquery.cookie.min.js"></script>
+		<script src="vendor/popper/umd/popper.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/common/common.min.js"></script>
+		<script src="vendor/jquery.validation/jquery.validate.min.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.min.js"></script>
+		<script src="vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
+		<script src="vendor/isotope/jquery.isotope.min.js"></script>
+		<script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+		<script src="vendor/vide/jquery.vide.min.js"></script>
+		<script src="vendor/vivus/vivus.min.js"></script>
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+		
+		<!-- Current Page Vendor and Views -->
+		<script src="vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+		<script src="vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+		<!-- Current Page Vendor and Views -->
+		<script src="vendor/nivo-slider/jquery.nivo.slider.min.js"></script>
+		<script src="js/views/view.contact.js"></script>
+
+		<!-- Demo -->
+		<script src="js/demos/demo-construction.js"></script>	
+		
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+
+
+
+	
+
+
+	</body>
+</html>
